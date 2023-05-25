c743d6800345ba2ca931b4ece43a47bdf464fffd
luongtt <trantienluong97@gmail.com>
2023-05-25 22:13:33 +0700

    prepare query avoid injection

--- com/everything/game/Model.java
+++ com/everything/game/Model.java
@@ -1,9 +1,11 @@
 package com.everything.game;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /*
@@ -11,25 +13,30 @@ import java.util.Map;
  * parent class support ORM Object–relational mapping
  */
 public class Model {
-    // TODO: check sql injection
     private ArrayList<String> whereQuery;
     private ArrayList<String> orderQuery;
-    Map<String, ArrayList<String>> whereInQuery;
     private String selectQuery;
     private int limit;
     protected String table;
+    private ArrayList<Object> parameters;
 
     public Model() {
         this.cleanQuery();
     }
     
-    public Model where(String query) {
-        this.whereQuery.add(query);
+    public Model where(String field, Object value) {
+        this.whereQuery.add(field + " = ?");
+        this.parameters.add(value);
         return this;
     }
 
-    public Model whereIn(String field, ArrayList<String> values) {
-        this.whereInQuery.put(field, values);
+    public Model whereIn(String field, List<String> values) {
+        ArrayList<String> inClause = new ArrayList<String>();
+        for (int i = 0; i < values.size(); i++) {
+            inClause.add("?");
+            this.parameters.add(values.get(i));
+        }
+        this.whereQuery.add(field + " IN (" + String.join(",", inClause) + ")");
         return this;
     }
 
@@ -63,8 +70,10 @@ public class Model {
             String query = this.makeQuery(String.format("SELECT %s FROM `%s`", this.selectQuery, this.table));
             if (this.limit > -1)
                 query += " LIMIT " + this.limit;
+            PreparedStatement statement = MySQLManager.conn.prepareStatement(query);
+            this.setParameter(statement);
             this.cleanQuery();
-            ResultSet resultSet = MySQLManager.stat.executeQuery(query);
+            ResultSet resultSet = statement.executeQuery();
             ArrayList<Map<String, Object>> rList = new ArrayList<>();
             while (resultSet.next())
                 rList.add(this.makeResult(resultSet));
@@ -79,8 +88,10 @@ public class Model {
         try {
             String query = this.makeQuery(String.format("SELECT %s FROM `%s`", this.selectQuery, this.table));
             query += " LIMIT 1";
+            PreparedStatement statement = MySQLManager.conn.prepareStatement(query);
+            this.setParameter(statement);
             this.cleanQuery();
-            ResultSet resultSet = MySQLManager.stat.executeQuery(query);
+            ResultSet resultSet = statement.executeQuery();
             if (resultSet.next())
                 return this.makeResult(resultSet);
             return null;
@@ -122,21 +133,15 @@ public class Model {
 
     private void cleanQuery() {
         this.whereQuery = new ArrayList<String>();
-        this.whereInQuery = new HashMap<>();
         this.orderQuery = new ArrayList<String>();
         this.selectQuery = "*";
         this.limit = -1;
+        this.parameters = new ArrayList<Object>();
     }
 
     private String makeQuery(String query) {
         if (this.whereQuery.size() > 0)
             query +=  " WHERE " + String.join(" AND ", this.whereQuery);
-        if (this.whereInQuery.size() > 0)
-            for (Map.Entry<String, ArrayList<String>> entry : this.whereInQuery.entrySet()) {
-                String key = entry.getKey();
-                ArrayList<String> values = entry.getValue();
-                query += String.format(" WHERE %s IN (%s)", key, String.join(",", values));
-            }
         if (this.orderQuery.size() > 0)
             for (String entry : this.orderQuery) {
                 query += " ORDER BY " + entry;
@@ -154,5 +159,12 @@ public class Model {
             resultMap.put(columnName, value);
         }
         return resultMap;
+    }
+
+    private void setParameter(PreparedStatement statement) throws Exception {
+        for (int i = 0; i < this.parameters.size(); i++) {
+            Object value = this.parameters.get(i);
+            statement.setObject(i + 1, value);
+        }
     }
 }
